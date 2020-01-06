9b7a061f155a88e3b72b7a8938bd6c06f5b86bd0
Erico Nogueira <ericonr@localhost.localdomain>
2020-01-06 13:14:45 -0300

    Update void "installer", add changes for Void session.
    Installer: has functions for each part of the environment, still lacks a
    way to run all function automatically.
    
    Emacs: move from the en-US dictionary, which isn't available on Void.
    
    Startsway: script to add all the stuff sway needs to start when the
    system doesn't do it for you.

--- distros/void.sh
+++ distros/void.sh
@@ -1,37 +1,122 @@
 #!/usr/bin/env bash
 
-xbps-install -S \
-	Waybar \
-	alacritty \
-	bat \
-	bmon \
-	breeze-snow-cursor-theme \
-	brightnessctl \
-	emacs-gtk2 \
-	fish-shell \
-	fzf \
-	git \
-	go \
-	grim \
-	htop \
-	hunspell \
-	hunspell-en_US \
-	hunspell-pt_BR \
-	iwd \
-	jq \
-	liberation-fonts \
-	mdcat \
-	neovim \
-	python3 \
-	redshift \
-	ripgrep \
-	sddm \
-	slurp \
-	starship \
-	stow \
-	sway \
-	swayidle \
-	swaylock \
-	tmux \
-	udisks2 \
+function print_help () {
+	echo Look inside for available stuff
+}
+
+function base () {
+	xbps-install -S base-system refind udisks2
+}
+
+function wm () {
+	xbps-install -S \
+		Waybar \
+		alacritty \
+		breeze-snow-cursor-theme \
+		brightnessctl \
+		fzf \
+		git \
+		go \
+		grim \
+		jq \
+		liberation-fonts-ttf \
+		redshift \
+		slurp \
+		sway \
+		swayidle \
+		swaylock \
+		wl-clipboard \
+		wofi
+}
+
+function term () {
+	xbps-install -S \
+		bat \
+		bmon \
+		fish-shell \
+		htop \
+		mdcat \
+		neovim \
+		python3 \
+		ripgrep \
+		starship \
+		stow \
+		tmux
+}
+
+function kicad () {
+	xbps-install -S \
+		kicad \
+		kicad-footprints \
+		kicad-library \
+		kicad-packages3D \
+		kicad-symbols \
+		kicad-templates
+}
+
+function dev () {
+	xbps-install -S \
+		arduino-cli \
+		clang \
+		go
+}
+
+function emacs () {
+	xbps-install -S \
+		emacs-gtk2 \
+		hunspell \
+		hunspell-en_US \
+		hunspell-pt_BR
+}
+
+function intel () {
+	xbps-install -S \
+		intel-gpu-tools \
+		iwd \
+		libva-intel-driver \
+		intel-media-driver \
+		mesa-intel-dri
+}
+
+function fonts () {
+	xbps-install -S noto-fonts-ttf noto-fonts-emoji
+}
+
+function qt5 () {
+	xbps-install -S qt5-wayland qt5ct
+}
+
+function qute () {
+	qt5
+	xbps-install -S qutebrowser pdf.js
+}
+
+function mozilla () {
+	xbps-install -S firefox thunderbird
+}
+
+function libreoffice () {
+	xbps-install -S \
+		libreoffice \
+		libreoffice-i18n-en-US \
+		libreoffice-i18n-pt-BR
+}
+
+function pdf () {
+	xbps-install -S zathura zathura-pdf-poppler
+}
+
+# Run function from script:
+# https://stackoverflow.com/questions/8818119/how-can-i-run-a-function-from-a-script-in-command-line
+# Check if the function exists (bash specific)
+if declare -f "$1" > /dev/null
+then
+  # call arguments verbatim
+  "$@"
+else
+  # Show a helpful error
+  echo "'$1' is not a known function name" >&2
+  print_help
+  exit 1
+fi
 
