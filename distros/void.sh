b74fe209b537087df0588009f9ac8fb9bd73014e
Érico Rolim <erico.erc@gmail.com>
2020-01-06 02:31:43 -0300

    Add script for Void Linux installation.

---
+++ distros/void.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+xbps-install -S \
+	Waybar \
+	alacritty \
+	bat \
+	bmon \
+	breeze-snow-cursor-theme \
+	brightnessctl \
+	emacs-gtk2 \
+	fish-shell \
+	fzf \
+	git \
+	go \
+	grim \
+	htop \
+	hunspell \
+	hunspell-en_US \
+	hunspell-pt_BR \
+	iwd \
+	jq \
+	liberation-fonts \
+	mdcat \
+	neovim \
+	python3 \
+	redshift \
+	ripgrep \
+	sddm \
+	slurp \
+	starship \
+	stow \
+	sway \
+	swayidle \
+	swaylock \
+	tmux \
+	udisks2 \
+
