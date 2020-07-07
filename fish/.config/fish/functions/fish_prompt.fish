064c9b09bb133978ad52bb48b391f29c30e4d7ce
Érico Rolim <erico.erc@gmail.com>
2020-07-07 18:51:31 -0300

    Add scripts for chrooting into a glibc boot env.
    - fish_prompt: to indicate that one is inside the chrooted fs
    
    - launch-chroot: chroots into the folder, can be turned into C program
    at some point to become setuid
    
    - mount-zvol: commmand to mount zvols that have mountpoint= set
    
    - chroot-mnt service: starts up the mounts, kills them with `sv down`.
    service-and-tz.sh script was updated to install it

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,1 +1,5 @@
-source ("starship" init fish --print-full-init | psub)
+if [ $CHROOTED ]
+	alias fish_prompt='echo "[chroot]" (pwd) "> "'
+else
+	source ("starship" init fish --print-full-init | psub)
+end
--- void/void.d/system/service-and-tz.sh
+++ void/void.d/system/service-and-tz.sh
@@ -10,15 +10,17 @@ fi
 
 echo "Installing services"
 for service in \
-	alsa bluetoothd boltd chronyd dbus dhcpcd elogind iwd popcorn snooze-daily
+	bluetoothd boltd chronyd dbus dhcpcd elogind iwd popcorn snooze-daily
 do
-	if [ -r "/etc/sv/${service}" ] ; then
+	if [ -r "$MOUNTDIR/etc/sv/${service}" ] ; then
 		ln -s "/etc/sv/${service}" "$SERVICES"
 		echo "Installed service ${service}"
 	else
 		echo "Service ${service} not found"
 	fi
 done
+
+cp -r etc-x86/sv/chroot-mnt "$SERVICES"
 
 echo "Setting timezone"
 ln -s /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
