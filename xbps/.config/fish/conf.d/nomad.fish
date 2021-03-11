51efcfead72327053461cc5a431882a9bd03f466
Érico Nogueira <erico.erc@gmail.com>
2021-03-11 02:33:57 -0300

    Create xbps/ and stick package management stuff there.
    Also put nomad functions.

---
+++ xbps/.config/fish/conf.d/nomad.fish
@@ -0,0 +1,14 @@
+set -x NOMAD_NAMESPACE '*'
+set -x NOMAD_ADDR "https://nomad.s.voidlinux.org"
+set -x VAULT_ADDR "https://vault.s.voidlinux.org"
+
+function vault-login
+	vault login -method=ldap
+	# can take username=$username parameter
+	#expires in one day
+end
+
+function nomad-token
+	set -x NOMAD_TOKEN (vault read -field secret_id nomad/creds/apps-admin)
+	#expires in one hour
+end
